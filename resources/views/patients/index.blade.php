d81fb6cf70cc891d8655f24dade816e7933a55e9
oscarzavala <ozavala@gmail.com>
2020-09-26 06:05:34 -0300

    Fundamentos Tailwindcss 2

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -13,31 +13,50 @@
           <span class="block text-gray-700 text-center bg-gray-300 px-4 py-2 mt-2">
             h2:Testiando Tailwind h1=h2
           </span>
-          <span class="block text-gray-700 text-center bg-gray-300 px-4 py-2 mt-2">
-            <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
-              Botón
+          <span class="block text-center px-4 py-2 mt-2">
+            <button class="bg-blue-500 border-2 border-blue-500 hover:border-blue-700 text-white font-bold mx-4 py-2 px-4 rounded-lg hover:bg-blue-700">
+              Botón 1
+            </button>
+            <button class="bg-red-500 border-2 border-red-500 hover:border-red-700 text-white font-bold mx-4 py-2 px-4 rounded-lg hover:bg-red-700">
+              Botón 2
+            </button>
+            <button class="bg-transparent border-2 border-blue-500 rounded-lg hover:bg-blue-500 text-blue-700 font-bold hover:text-white mx-4 py-2 px-4">
+              Botón 3
             </button>
           </span>
         </div>
-        <div class="border mx-4 my-4 px-2 py-2">
+        <div class="border mx-4 my-4 px-4 py-4">
+            <div class="overflow-y-scroll h-80 flex justify-center">
                 <table class="table-auto">
-                    <head>
-                        <tr class="text-center">
-                            <td class="border px-4 py-2 font-bold">Nombre</td>
-                            <td class="border px-4 py-2 font-bold">Edad</td>
-                            <td class="border px-4 py-2 font-bold">Genero</td>
-                        </tr>
-                    </head>
-                    <body>
-                        @foreach($patients as $patient)
+                    <thead>
                         <tr>
-                            <td class="border px-4 py-2">{{ $patient->name }}</td>
-                            <td class="border px-4 py-2">{{ $patient->age }}</td>
-                            <td class="border px-4 py-2">{{ $patient->gender }}</td>
+                            <th class="px-4 py-2 font-bold">Index</th>
+                            <th class="px-4 py-2 font-bold">Nombre</th>
+                            <th class="px-4 py-2 font-bold">Edad</th>
+                            <th class="px-4 py-2 font-bold">Genero</th>
                         </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($patients as $patient)
+                            @if ($loop->index % 2 == 0)
+                            <tr>
+                                <td class="border px-4 py-2">{{ $loop->index }}</td>
+                                <td class="border px-4 py-2">{{ $patient->name }}</td>
+                                <td class="border px-4 py-2">{{ $patient->age }}</td>
+                                <td class="border px-4 py-2">{{ $patient->gender }}</td>
+                            </tr>
+                            @else
+                            <tr class="bg-gray-200">
+                                <td class="border px-4 py-2">{{ $loop->index }}</td>
+                                <td class="border px-4 py-2">{{ $patient->name }}</td>
+                                <td class="border px-4 py-2">{{ $patient->age }}</td>
+                                <td class="border px-4 py-2">{{ $patient->gender }}</td>
+                            </tr>
+                            @endif
                         @endforeach
-                    </body>
+                    </tbody>
                 </table>
+            </div>
         </div>
     </div>
 
