1458269445da1cb1f047bba82b984da9d57ebaab
oscarzavala <ozavala@gmail.com>
2020-09-27 15:15:31 -0300

    Patients->index

--- app/Http/Livewire/PatientShow.php
+++ app/Http/Livewire/PatientShow.php
@@ -8,11 +8,11 @@ use App\Models\Patient;
 class PatientShow extends Component
 {
     public $search;
-    
+
     public function render()
     {
         return view('livewire.patient-show', [
-            'users' => Patient::where('name','LIKE','%'.$this->search.'%')->get(),
+            'patients' => Patient::where('name','LIKE','%'.$this->search.'%')->get(),
         ]);
     }
 }
--- resources/views/livewire/patient-show.blade.php
+++ resources/views/livewire/patient-show.blade.php
@@ -1,9 +1,40 @@
-<div>
-    <input wire:model="search" type="text" placeholder="Search users..."/>
-
-    <ul>
-        @foreach($users as $user)
-            <li>{{ $user->name }}</li>
-        @endforeach
-    </ul>
+<div class="container border mx-auto my-4">
+    <div class="border mx-4 my-4 px-2 py-2 rounded-lg">
+      <span class="block text-gray-700 text-center bg-gray-100 px-4 py-2">
+        <input class="border mx-4 my-4 px-2 py-2 rounded-lg" wire:model="search" type="text" placeholder="Buscar de Pacientes..."/>
+      </span>
+    </div>
+    <div class="border mx-4 my-4 px-4 py-4 rounded-lg">
+        <div class="overflow-y-scroll h-80 flex justify-center">
+            <table class="table-auto">
+                <thead>
+                    <tr>
+                        <th class="px-4 py-2 font-bold">Index</th>
+                        <th class="px-4 py-2 font-bold">Nombre</th>
+                        <th class="px-4 py-2 font-bold">Edad</th>
+                        <th class="px-4 py-2 font-bold">Genero</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($patients as $patient)
+                        @if ($loop->index % 2 == 0)
+                        <tr>
+                            <td class="border px-4 py-2">{{ $loop->index }}</td>
+                            <td class="border px-4 py-2">{{ $patient->name }}</td>
+                            <td class="border px-4 py-2">{{ $patient->age }}</td>
+                            <td class="border px-4 py-2">{{ $patient->gender }}</td>
+                        </tr>
+                        @else
+                        <tr class="bg-gray-200">
+                            <td class="border px-4 py-2">{{ $loop->index }}</td>
+                            <td class="border px-4 py-2">{{ $patient->name }}</td>
+                            <td class="border px-4 py-2">{{ $patient->age }}</td>
+                            <td class="border px-4 py-2">{{ $patient->gender }}</td>
+                        </tr>
+                        @endif
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
