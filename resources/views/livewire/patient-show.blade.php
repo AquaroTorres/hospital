55676839be0dd5c42fdd427819e3ebf9027af728
oscarzavala <ozavala@gmail.com>
2020-09-27 23:21:00 -0300

    Fix -> index - create

--- resources/views/livewire/patient-show.blade.php
+++ resources/views/livewire/patient-show.blade.php
@@ -1,15 +1,19 @@
-<div class="container border mx-auto my-4">
-    <div class="border mx-4 my-4 px-2 py-2 rounded-lg">
-      <span class="block text-gray-700 text-center bg-gray-100 px-4 py-2">
-        <button class="bg-blue-500 border-2 border-blue-500 hover:border-blue-700 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700" onclick="window.location.href='{{ route('patients.create') }}'">
-          Agregar Paciente
-        </button>
-        <input class="border mx-4 my-4 px-2 py-2 rounded-lg" wire:model="search" type="text" placeholder="Buscar de Pacientes..."/>
-      </span>
+<div class="container bg-white border mx-auto my-4">
+
+    <div class="flex justify-star mx-2 mb-0 mt-4 px-2 py-0">
+      <p class="text-lg font-semibold">Listado de Pacientes:</p>
     </div>
-    <div class="border mx-4 my-4 px-4 py-4 rounded-lg">
+
+    <div class="border shadow-xl mx-4 mt-0 mb-16 px-4 py-8 rounded-lg">
+      <div class="flex justify-center bg-white mx-0 mb-4 px-0 py-0">
+        <button class="bg-blue-500 border-2 border-blue-500 hover:border-blue-700 text-white font-bold py-2 px-2 rounded-lg hover:bg-blue-700" onclick="window.location.href='{{ route('patients.create') }}'">
+          Crear Paciente
+        </button>
+        <input size="40" class="border ml-2 my-0 px-2 py-2 rounded-lg" wire:model="search" type="text" placeholder="Buscar de Pacientes..."/>
+      </div>
+
         <div class="overflow-y-scroll h-80 flex justify-center">
-            <table class="table-auto">
+            <table class="table-auto text-left">
                 <thead>
                     <tr>
                         <th class="px-4 py-2 font-bold">Index</th>
@@ -22,17 +26,17 @@
                     @foreach($patients as $patient)
                         @if ($loop->index % 2 == 0)
                         <tr>
-                            <td class="border px-4 py-2">{{ $loop->index }}</td>
-                            <td class="border px-4 py-2">{{ $patient->name }}</td>
-                            <td class="border px-4 py-2">{{ $patient->age }}</td>
-                            <td class="border px-4 py-2">{{ $patient->gender }}</td>
+                            <td class="px-4 py-2">{{ $loop->index }}</td>
+                            <td class="px-4 py-2">{{ $patient->name }}</td>
+                            <td class="px-4 py-2">{{ $patient->age }}</td>
+                            <td class="px-4 py-2">{{ $patient->gender }}</td>
                         </tr>
                         @else
                         <tr class="bg-gray-200">
-                            <td class="border px-4 py-2">{{ $loop->index }}</td>
-                            <td class="border px-4 py-2">{{ $patient->name }}</td>
-                            <td class="border px-4 py-2">{{ $patient->age }}</td>
-                            <td class="border px-4 py-2">{{ $patient->gender }}</td>
+                            <td class="px-4 py-2">{{ $loop->index }}</td>
+                            <td class="px-4 py-2">{{ $patient->name }}</td>
+                            <td class="px-4 py-2">{{ $patient->age }}</td>
+                            <td class="px-4 py-2">{{ $patient->gender }}</td>
                         </tr>
                         @endif
                     @endforeach
--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -6,9 +6,8 @@
     </x-slot>
 
     <div class="container bg-white border mx-auto my-4">
-
-        <div class="flex justify-star mx-4 mb-0 mt-4 px-2 py-0">
-          <p class="text-lg font-semibold">Información del Paciente:</p>
+        <div class="flex justify-star mx-2 mb-0 mt-4 px-2 py-0">
+          <p class="text-lg font-semibold">Ingrese información del Paciente:</p>
         </div>
 
         <div class="flex justify-around shadow-xl border mx-4 mt-0 mb-16 px-2 py-2 rounded-lg">
@@ -26,7 +25,6 @@
           			<input type="text" class="py-1 px-1 border rounded-lg" id="forGenero" name="gender" required="required">
           		</fieldset>
           </span>
-
           <span class="block text-gray-700 px-0 py-2 mr-4 my-4">
             <fieldset class="flex flex-col my-4 mx-4">
               <label for="forNombre">Rut</label>
@@ -37,18 +35,17 @@
               <input type="text" class="py-1 px-1 border rounded-lg" id="forEdad" name="age" required="required">
             </fieldset>
             <fieldset class="flex flex-col mb-4 mx-4">
-              <label for="forGenero">Genero</label>
+              <label for="forGenero">Género</label>
               <input type="text" class="py-1 px-1 border rounded-lg" id="forGenero" name="gender" required="required">
             </fieldset>
           </span>
-
           <span class="block text-gray-700 px-0 py-2 mr-4 my-4">
             <fieldset class="flex flex-col my-4 mx-4">
               <label for="forNombre">Dirección</label>
               <input type="text" size="25" class="py-1 px-1 border rounded-lg" id="forNombre" name="name" required="required">
             </fieldset>
             <fieldset class="flex flex-col mb-4 mx-4">
-              <label for="forEdad">Comúna</label>
+              <label for="forEdad">Comuna</label>
               <input type="text" class="py-1 px-1 border rounded-lg" id="forEdad" name="age" required="required">
             </fieldset>
             <fieldset class="flex flex-col mb-4 mx-4">
@@ -56,13 +53,12 @@
               <input type="text" class="py-1 px-1 border rounded-lg" id="forGenero" name="gender" required="required">
             </fieldset>
           </span>
-
           <span class="block text-gray-700 px-0 py-2 mr-4 my-4">
             <fieldset class="flex flex-col my-4 mx-4">
         			<label for="forUsers">Asignar Alergias:</label>
         			<select style="width: 300px;" size="7" multiple class="custom-select border" id="forUsers" name="patients[]">
         				@foreach($allergies as $allergy)
-        					<option value="{{ $allergy->id }}">{{ $allergy->id.'      '.$allergy->name }}</option>
+        					<option value="{{ $allergy->id }}">{{ $allergy->id.' '.$allergy->name }}</option>
         				@endforeach
         			</select>
         			<small class="form-text text-muted">
@@ -70,8 +66,18 @@
         			</small>
         		</fieldset>
           </span>
-
         </div>
+    </div>
+
+    <div class="container bg-transparent mx-auto my-8">
+      <div class="flex justify-star mx-0 my-0 px-0 py-0">
+        <button class="bg-blue-500 border-2 border-blue-500 hover:border-blue-700 text-white font-bold rounded-lg hover:bg-blue-700 mr-12 my-0 py-2 px-4">
+          Guardar
+        </button>
+        <button class="bg-red-500 border-2 border-red-500 hover:border-red-700 text-white font-bold rounded-lg hover:bg-red-700 ml-12 my-0 py-2 px-4">
+          Volver
+        </button>
+      </div>
     </div>
 
 </x-app-layout>
